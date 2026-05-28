43423e315c28538e615ed0876854e4dbe054e839
Patrick Lostaunau <jplosta01@gmail.com>
2026-05-27 19:14:27 -0500

    Initial Commit

---
+++ ControlCash/ControlCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlCashApp.swift
+//  ControlCash
+//
+//  Created by Patrick Lostaunau on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ControlCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
